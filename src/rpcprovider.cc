22cc7d58f27b237eaf031f4af9e27a0ed6ad193a
Lee <759859067@qq.com>
2025-06-05 17:07:01 +0800

    feat-OnMessage函数rpc调用回调函数中根据通讯格式解析获取调用信息,并完成反序列化

--- src/rpcprovider.cc
+++ src/rpcprovider.cc
@@ -1,7 +1,7 @@
 #include "rpcprovider.h"
 #include "mprpcapplication.h"
 #include <functional>
-
+#include "rpcheader.pb.h"
 using namespace std;
 
 /*
@@ -51,12 +51,89 @@ void RpcProvider::Run()
     m_eventLoop.loop();
 }
 
-void RpcProvider::OnConnecction(const muduo::net::TcpConnectionPtr&)
+void RpcProvider::OnConnecction(const muduo::net::TcpConnectionPtr& conn)
 {
-
+    if(!conn->connected())
+    {
+        //rpc client连接断开，关闭文件描述符fd
+        conn->shutdown();
+    }
+}
+/*
+定义通讯的数据格式
+框架内部Provider和Consumer要协商好通信的protobuf的数据类型
+service_name method_name args 定义proto的message类型进行序列化和反序列化
+header_size(4个字节)+header_str+args
+header_str.size()+(service_name+method_name+args.size())+args防止粘包问题
+string insert+copy
+*/
+//如果远程有一个rpc调用的服务 那么这个函数就会响应
+void RpcProvider::OnMessage(const muduo::net::TcpConnectionPtr& conn,muduo::net::Buffer* buffer,muduo::Timestamp)
+{
+    //接收远程调用请求的字符流  Login args
+    string recv_buf=buffer->retrieveAllAsString();
+    //拷贝4个字节的内容
+    uint32_t header_size;
+    recv_buf.copy((char *)&header_size,4,0);
+    string rpc_header_str=recv_buf.substr(4,header_size);
+    //从字节流或者字符流反序列化
+    mprpc::RpcHeader rpcheader;
+    string service_name;
+    string method_name;
+    uint32_t args_size;
+    if(rpcheader.ParseFromString(rpc_header_str))
+    {
+        //反序列化成功
+        service_name=rpcheader.service_name();
+        method_name=rpcheader.method_name();
+        args_size=rpcheader.args_size();
+    }
+    else
+    {
+        cout<<"rpc_header_str :"<<rpc_header_str<<"ParseFromString error!"<<endl;
+        return;
+    }
+    string args_str=recv_buf.substr(4+header_size,args_size);
+    cout<<"======================================="<<endl;
+    cout<<"header_size:"<<header_size<<endl;
+    cout<<"rpc_header_str:"<<rpc_header_str<<endl;
+    cout<<"service_name:"<<service_name<<endl;
+    cout<<"method_name:"<<method_name<<endl;
+    cout<<"args_size:"<<args_size<<endl;
+    cout<<"args_str:"<<args_str<<endl;
+    cout<<"======================================="<<endl;
+    //获取Method和Service对象
+    auto it=m_serviceMap.find(service_name);
+    if(it==m_serviceMap.end())
+    {
+        cout<<"service_name:"<<service_name<<" is not exist!"<<endl;
+        return;
+    }
+    
+    auto mit=it->second.m_methodMap.find(method_name);
+    if(mit==it->second.m_methodMap.end())
+    {
+        cout<<"method_name:"<<method_name<<" is not exist!"<<endl;
+        return;
+    }
+    google::protobuf::Service* service=it->second.m_service; //UserService class
+    const google::protobuf::MethodDescriptor* method=mit->second;//Login()
+    //生成获取rqc请求的request和respone
+    google::protobuf::Message* request=service->GetRequestPrototype(method).New();
+    google::protobuf::Message* response=service->GetResponsePrototype(method).New();
+    if(!request->ParseFromString(args_str))
+    {
+        cout<<"request parse error,content:"<<args_str<<endl;
+        return;
+    }
+    //给下面的method绑定一个Closure回调
+    google::protobuf::NewCallback();
+    //根据远程调用的rpc请求，调用当前的rpc发布的方法
+    //UserService.Login(controller,request,respones,done)
+    service->CallMethod(method,nullptr,request,response,);
 }
 
-void RpcProvider::OnMessage(const muduo::net::TcpConnectionPtr&,muduo::net::Buffer*,muduo::Timestamp)
+void RpcProvider::SendRpcResponse(const muduo::net::TcpConnectionPtr&,google::protobuf::Message*)
 {
 
 }
